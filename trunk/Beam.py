aba0d210168984b214cbd1386e8ee1f1d15fb2e0
horia.uifaleanu@gmail.com <horia.uifaleanu@gmail.com@b2126cb8-cc4b-07e0-b2cb-7117c13d5ace>
2014-11-15 17:40:30 +0000

    Moving the About and Close Dialogs into separate files (inside bin/dialogs folder)

--- trunk/Beam.py
+++ trunk/Beam.py
@@ -13,7 +13,8 @@ from bin import HandleData, ParseSettings
 from bin.Preferences import Preferences
 
 from bin.dialogs.helpdialog import HelpDialog
-
+from bin.dialogs import aboutdialog
+from bin.dialogs import closedialog
 
 ##################################################
 # MAIN WINDOW - FRAME
@@ -252,9 +253,12 @@ class Frame(wx.Frame):
     def fullScreen(self, event):
 	self.ShowFullScreen(not self.IsFullScreen())
 #
-# CLOSE DIALOG
+# Show 'Close dialog
 #
     def OnClose(self, event):
+        closedialog.ShowCloseDialog(self)
+        
+    def OnClose1(self, event):
         dlg = wx.MessageDialog(self,
             "Do you really want to close this application?",
             "Confirm Exit", wx.OK|wx.CANCEL|wx.ICON_QUESTION)
@@ -269,47 +273,13 @@ class Frame(wx.Frame):
 	    self.Destroy()
 
 #
-# ABOUT DIALOG
-# A message dialog box with an OK button. wx.OK is a standard ID in wxWidgets.
+# Show 'About Dialog'
 #
     def OnAbout(self, event):
-        description = """Beam is an advanced live media extractor for Unix, 
-Windows and Mac operating system. Features include a
-scalable display for TV or projector, customizable 
-backgrounds and text, possibility to detect cortinas 
-and display the next tanda.
-"""
-        licence = """Beam is free software; you can redistribute it and/or 
-modify it under the terms of the GNU General Public 
-License as published by the Free Software Foundation; 
-either version 2 of the License, or (at your option) any
-later version.
-
-Beam is distributed in the hope that it will be useful, 
-but WITHOUT ANY WARRANTY; without even the 
-implied warranty of MERCHANTABILITY or FITNESS 
-FOR A PARTICULAR PURPOSE.  See the GNU General 
-Public License for more details. You should have 
-received a copy of the GNU General Public License 
-along with Beam; if not, write to the Free Software 
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
-MA  02111-1307  USA"""
-
-        info = wx.AboutDialogInfo()
-
-        info.SetIcon(wx.Icon(self.icon, wx.BITMAP_TYPE_PNG))
-        info.SetName('Beam')
-        info.SetVersion('0.1')
-        info.SetDescription(description)
-        info.SetCopyright('(C) 2014 - 2015 Mikael Holber')
-        info.SetWebSite('http://www.beam-project.com')
-        info.SetLicence(licence)
-        info.AddDeveloper('Mikael Holber and Horia Uifaleanu')
-	info.AddArtist('Artwork by Andreea Vlad - www.andreeavlad.com')
-        wx.AboutBox(info)
-
+        aboutdialog.ShowAboutDialog(self)
+        
 #
-# Open Help
+# Show 'Help'
 #
     def OnHelp(self, event):
         help_dialog = HelpDialog(None, self)
