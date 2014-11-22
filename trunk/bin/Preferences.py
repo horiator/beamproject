f6f9219b9c5d0b7d9a11703e34c5b11d1ac777c4
horia.uifaleanu@gmail.com <horia.uifaleanu@gmail.com@b2126cb8-cc4b-07e0-b2cb-7117c13d5ace>
2014-11-22 01:53:12 +0000

    Moved EditLayout and EditRule dialogs into separate classes

--- trunk/bin/Preferences.py
+++ trunk/bin/Preferences.py
@@ -27,7 +27,8 @@ import wx.lib.flatnotebook as fnb
 import os
 
 from bin.beamsettings import *
-
+from bin.dialogs.editlayoutdialog import EditLayoutDialog
+from bin.dialogs.editruledialog import EditRuleDialog
 
 #
 # Build main preferences Window
@@ -109,7 +110,7 @@ class Preferences(wx.Dialog):
 
     def LayoutSettings(self, notebook):
         
-        panel = wx.Panel(self)
+        panel = wx.Panel(notebook)
         self.DisplayRows = []
 
         self.AddLayout  = wx.Button(panel, label="Add")
@@ -153,7 +154,7 @@ class Preferences(wx.Dialog):
 
     def RulesSettings(self, notebook):
     
-        panel = wx.Panel(self)
+        panel = wx.Panel(notebook)
         
         self.RuleRows = []
         
@@ -206,7 +207,7 @@ class Preferences(wx.Dialog):
 #
     def onApply(self, e):
         # Get Settings
-        beamSettings._moduleSelected         = self.Dropdown.GetValue()  
+        beamSettings._moduleSelected     = self.Dropdown.GetValue()  
         beamSettings._updateTimer        = int(self.TimerText.GetValue())
         beamSettings._maxTandaLength     =  int(self.TandaLength.GetValue())
             #try:
@@ -242,13 +243,13 @@ class Preferences(wx.Dialog):
 #
 
     def OnAddLayout(self, event):
-        self.EditLayout = EditLayout(self, len(self.DisplayRows), "Add layout item")
+        self.EditLayout = EditLayoutDialog(self, len(self.DisplayRows), "Add layout item")
         self.EditLayout.Show()
 
     def OnEditLayout(self, event):
         RowSelected = self.LayoutList.GetSelection()
         if RowSelected>-1:
-            self.EditLayout = EditLayout(self, RowSelected, "Add layout item")
+            self.EditLayout = EditLayoutDialog(self, RowSelected, "Add layout item")
             self.EditLayout.Show()
         
     def OnDelLayout(self, event):
@@ -266,17 +267,15 @@ class Preferences(wx.Dialog):
 #
 # RULE BUTTONS
 #
-#
             
     def OnAddRule(self, event):
-        self.EditRule = EditRule(self, self.RuleList.GetCount(), "Add rule")
+        self.EditRule = EditRuleDialog(self, self.RuleList.GetCount(), "Add rule")
         self.EditRule.Show()
-
         
     def OnEditRule(self, event):
         RowSelected = self.RuleList.GetSelection()
         if RowSelected>-1:
-            self.EditRule = EditRule(self, RowSelected, "Edit rule")
+            self.EditRule = EditRuleDialog(self, RowSelected, "Edit rule")
             self.EditRule.Show()
        
     def OnDelRule(self, event):
@@ -291,334 +290,3 @@ class Preferences(wx.Dialog):
             if result == wx.ID_OK:
                 self.Rules.pop(RowSelected)
                 self.BuildRuleList()
-
-
-
-#
-# LAYOUT WINDOW-CLASS
-#
-#
-
-class EditLayout(wx.Dialog):
-    def __init__(self, parent, RowSelected, mode):
-        self.EditLayoutDialog   = wx.Dialog.__init__(self, parent, title=mode, size=(570,210))
-        self.EditLayoutPanel        = wx.Panel(self)
-        self.parent             = parent
-        self.RowSelected        = RowSelected
-
-        self.ButtonSaveLayout   = wx.Button(self.EditLayoutPanel, label="Save")
-        self.ButtonCancelLayout     = wx.Button(self.EditLayoutPanel, label="Cancel")
-        self.ButtonSaveLayout.Bind(wx.EVT_BUTTON, self.OnSaveLayoutItem)
-        self.ButtonCancelLayout.Bind(wx.EVT_BUTTON, self.OnCancelLayoutItem)
-
-        Fonts   = ["Decorative","Default","Modern","Roman","Script","Swiss","Teletype"]
-        Weights     = ["Bold","Light","Normal"]
-        Styles  = ["Italic","Normal","Slant"]
-        
-        # Check if it is a new line
-        if self.RowSelected<len(beamSettings._myDisplaySettings):
-            # Get the properties of the selected item
-            self.Settings   = beamSettings._myDisplaySettings[self.RowSelected]
-        else:
-            # Create a new default setting
-            self.Settings   = ({"Field": "Artist[1]", "Font": "Default","Style": "Normal", "Weight": "Bold", "Size": 20, "FontColor": "(255,255,255,255)", "HideControl": "NextTanda[1]", "Position": [50,50], "Center": "yes"})
-
-        
-        # Define fields
-        self.LabelText          = wx.TextCtrl(self.EditLayoutPanel, size=(150,-1), value=self.Settings[u'Field'])
-        self.FontDropdown       = wx.ComboBox(self.EditLayoutPanel,value=self.Settings[u'Font'], choices=Fonts)
-        self.StyleDropdown      = wx.ComboBox(self.EditLayoutPanel,value=self.Settings[u'Style'], choices=Styles)
-        self.WeightDropdown     = wx.ComboBox(self.EditLayoutPanel,value=self.Settings[u'Weight'], choices=Weights)
-        self.SizeText           = wx.TextCtrl(self.EditLayoutPanel, value=str(self.Settings[u'Size']))
-        self.ColorField         = wx.ColourPickerCtrl(self.EditLayoutPanel)
-        self.HideText           = wx.TextCtrl(self.EditLayoutPanel, value=self.Settings[u'HideControl'])
-        self.VerticalPos        = wx.TextCtrl(self.EditLayoutPanel, value=str(self.Settings[u'Position'][0]))
-        self.HorizontalPos      = wx.TextCtrl(self.EditLayoutPanel, value=str(self.Settings[u'Position'][1]))
-        #Checkbox
-        self.CenterCheck        = wx.CheckBox(self.EditLayoutPanel, label="Center")
-        self.CenterCheck.Bind(wx.EVT_CHECKBOX, self.DisableHorizontalBox)
-        if self.Settings[u'Center']=="yes": 
-            self.CenterCheck.SetValue(True)
-            self.HorizontalPos.Enable(not self.CenterCheck.GetValue())
-        # Set color
-        self.ColorField.SetColour(eval(self.Settings[u'FontColor']))
-        
-        # Information area
-        InfoGrid    =   wx.FlexGridSizer(4, 5, 5, 5)
-        InfoGrid.AddMany ( [(wx.StaticText(self.EditLayoutPanel, label="Label"), 0, wx.EXPAND),
-                        (wx.StaticText(self.EditLayoutPanel, label="Font"), 0, wx.EXPAND),
-                        (wx.StaticText(self.EditLayoutPanel, label="Style"), 0, wx.EXPAND),
-                        (wx.StaticText(self.EditLayoutPanel, label="Weight"), 0, wx.EXPAND),
-                        (wx.StaticText(self.EditLayoutPanel, label="Size"), 0, wx.EXPAND),
-                        (self.LabelText, 0, wx.EXPAND),
-                        (self.FontDropdown, 0, wx.EXPAND),
-                        (self.StyleDropdown, 0, wx.EXPAND),
-                        (self.WeightDropdown, 0, wx.EXPAND),
-                        (self.SizeText, 0, wx.EXPAND),
-                        (wx.StaticText(self.EditLayoutPanel, label="Hide/Show"), 0, wx.EXPAND),
-                        (wx.StaticText(self.EditLayoutPanel, label="Color"), 0, wx.EXPAND),
-                        (wx.StaticText(self.EditLayoutPanel, label="Vertical"), 0, wx.EXPAND),
-                        (wx.StaticText(self.EditLayoutPanel, label="Horizontal"), 0, wx.EXPAND),
-                        (wx.StaticText(self.EditLayoutPanel, label=""), 0, wx.EXPAND),
-                        (self.HideText, 0, wx.EXPAND),
-                        (self.ColorField, 0, wx.EXPAND),
-                        (self.VerticalPos, 0, wx.EXPAND),
-                        (self.HorizontalPos, 0, wx.EXPAND),
-                        (self.CenterCheck, 0, wx.EXPAND)
-                        ])
-
-        self.vboxLayout = wx.BoxSizer(wx.VERTICAL)
-        self.hboxLayout = wx.BoxSizer(wx.HORIZONTAL)
-        
-        self.hboxLayout.Add((400, -1), 1, flag=wx.EXPAND | wx.ALIGN_RIGHT)
-        self.hboxLayout.Add(self.ButtonSaveLayout, flag=wx.LEFT | wx.BOTTOM | wx.TOP, border=10)
-        self.hboxLayout.Add(self.ButtonCancelLayout, flag=wx.LEFT | wx.BOTTOM | wx.TOP | wx.RIGHT, border=10)
-        
-        self.vboxLayout.Add(InfoGrid, flag=wx.LEFT | wx.BOTTOM | wx.TOP | wx.RIGHT, border=10)
-        self.vboxLayout.Add(self.hboxLayout)
-
-        self.EditLayoutPanel.SetSizer(self.vboxLayout)
-        
-    def DisableHorizontalBox(self, event):
-        self.HorizontalPos.Enable(not self.CenterCheck.GetValue())
-        
-    def OnSaveLayoutItem(self, event):
-        self.Settings[u'Field']         = self.LabelText.GetValue()
-        self.Settings[u'Font']      = self.FontDropdown.GetValue()  
-        self.Settings[u'Style']         = self.StyleDropdown.GetValue()
-        self.Settings[u'Weight']        = self.WeightDropdown.GetValue()
-        self.Settings[u'Size']      = int(self.SizeText.GetValue())
-        self.Settings[u'HideControl']   = self.HideText.GetValue()
-        self.Settings[u'FontColor']     = str(self.ColorField.GetColour())
-        self.Settings[u'Position']  = [int(self.VerticalPos.GetValue()), int(self.HorizontalPos.GetValue())]
-        if self.CenterCheck.GetValue():
-            self.Settings[u'Center']    = 'yes'
-        else:
-            self.Settings[u'Center']    = 'no' 
-        
-        # Save item into dictionary
-        if self.RowSelected+1 > len(beamSettings._myDisplaySettings):
-            beamSettings._myDisplaySettings.append(self.Settings)
-        else:
-            beamSettings._myDisplaySettings[self.RowSelected] = self.Settings
-        self.parent.BuildLayoutList()
-        self.Destroy()
-    
-    def OnCancelLayoutItem(self, event):
-        self.Destroy()
-
-
-#
-# Edit Rule class
-#
-#
-
-
-class EditRule(wx.Dialog):
-    def __init__(self, parent, RowSelected, mode):
-        self.EditRuleDialog     = wx.Dialog.__init__(self, parent, title=mode, size=(470,210))
-        self.EditRulePanel  = wx.Panel(self)
-        self.parent             = parent
-        self.RowSelected    = RowSelected
-        self.mode           = mode
-
-        self.ButtonSaveRule     = wx.Button(self.EditRulePanel, label="Save")
-        self.ButtonCancelRule   = wx.Button(self.EditRulePanel, label="Cancel")
-        self.ButtonSaveRule.Bind(wx.EVT_BUTTON, self.OnSaveRuleItem)
-        self.ButtonCancelRule.Bind(wx.EVT_BUTTON, self.OnCancelRuleItem)
-        self.InputFields    = ["Artist","Album","Title","Genre","Comment","Composer","Year"]
-        self.OutputFields   = ["Artist","Album","Title","Genre","Comment","Composer","Year", "Singer"]
-
-    # Check if it is a new line
-        if self.RowSelected<len(beamSettings._rules):
-            # Get the properties of the selected item
-            self.Settings   = beamSettings._rules[self.RowSelected]
-        else:
-            # Create a new default setting
-            self.Settings   = ({"Type": "Set", "Field1": "Comment","Field2": "Singer", "Active": "yes"})
-
-        # Build the static elements
-        self.InputID3Field      = wx.ComboBox(self.EditRulePanel,value=self.Settings[u'Field1'], choices=self.InputFields)
-        self.RuleSelectDropdown     = wx.ComboBox(self.EditRulePanel,value=self.Settings[u'Type'], choices=['Set','Cortina','Parse'])
-        self.RuleSelectDropdown.Bind(wx.EVT_COMBOBOX, self.ChangeRuleType)
-        self.RuleOrder          = wx.TextCtrl(self.EditRulePanel, value=str(self.RowSelected+1))
-        self.ActivateRule       = wx.CheckBox(self.EditRulePanel, label="Activate")
-        
-        if self.Settings[u'Active'] == "yes":
-            self.ActivateRule.SetValue(True)
-        
-        # Dynamic fields (Changes depending on RuleSelectDropdown)
-        self.DynamicFieldLabel1 = wx.StaticText(self.EditRulePanel, label="")
-        self.DynamicFieldLabel2 = wx.StaticText(self.EditRulePanel, label="")
-        self.TokenLabel         = wx.StaticText(self.EditRulePanel, label="Token")
-        self.TokenField         = wx.TextCtrl(self.EditRulePanel, value="")
-        
-        self.sizer1 = wx.BoxSizer(wx.HORIZONTAL)
-        self.sizer2 = wx.BoxSizer(wx.HORIZONTAL)
-        
-        InfoGrid    =   wx.FlexGridSizer(4, 4, 5, 5)
-        InfoGrid.AddMany ( [(wx.StaticText(self.EditRulePanel, label="Input ID3 tag"), 0, wx.EXPAND),
-                        (wx.StaticText(self.EditRulePanel, label="Rule type"), 0, wx.EXPAND),
-                        (self.DynamicFieldLabel1, 0, wx.EXPAND),
-                        (self.DynamicFieldLabel2, 0, wx.EXPAND),
-                        (self.InputID3Field, 0, wx.EXPAND),
-                        (self.RuleSelectDropdown, 0, wx.EXPAND),
-                        (self.sizer1, 0, wx.EXPAND),
-                        (self.sizer2, 0, wx.EXPAND),
-                        (wx.StaticText(self.EditRulePanel, label="Rule order"), 0, wx.EXPAND),
-                        (self.TokenLabel, 0, wx.EXPAND),
-                        (wx.StaticText(self.EditRulePanel, label=""), 0, wx.EXPAND),
-                        (wx.StaticText(self.EditRulePanel, label=""), 0, wx.EXPAND),
-                        (self.RuleOrder, 0, wx.EXPAND),
-                        (self.TokenField, 0, wx.EXPAND)
-                        ])
-                        
-        self.vbox = wx.BoxSizer(wx.VERTICAL)
-        self.hbox = wx.BoxSizer(wx.HORIZONTAL)
-        
-        self.EditRulePanel.SetSizer(self.vbox)
-        
-        self.hbox.Add((200, -1), 1, flag=wx.EXPAND | wx.ALIGN_RIGHT)
-        self.hbox.Add(self.ActivateRule, flag=wx.LEFT | wx.TOP, border=13)
-        self.hbox.Add(self.ButtonSaveRule, flag=wx.LEFT | wx.BOTTOM | wx.TOP, border=10)
-        self.hbox.Add(self.ButtonCancelRule, flag=wx.LEFT | wx.BOTTOM | wx.TOP | wx.RIGHT, border=10)
-        
-        self.vbox.Add(InfoGrid, flag=wx.LEFT | wx.BOTTOM | wx.TOP | wx.RIGHT, border=10)
-        self.vbox.Add(self.hbox)
-        
-        self.ChangeRuleType(self)
-    
-    
-    def ChangeRuleType(self, event):
-        RuleSelected = self.RuleSelectDropdown.GetValue()
-        ###########################################
-        if RuleSelected == 'Set':
-            self.DynamicFieldLabel1.SetLabel('Output field')
-            self.DynamicFieldLabel2.SetLabel('')
-            # Remove fields that are not to be shown
-            self.RemoveDynamicElements()        
-
-            self.TokenLabel.Hide() 
-            self.TokenField.Hide() 
-            
-            #Add correct fields
-            self.OutputField1       = wx.ComboBox(self.EditRulePanel,value="Artist", choices=self.OutputFields)
-            self.sizer1.Add(self.OutputField1)
-            
-            if self.Settings[u'Type'] == 'Set':
-                self.OutputField1.SetStringSelection(self.Settings[u'Field2'])
-            else:
-                self.OutputField1.SetStringSelection("Artist")
-                
-        ########################################
-        if RuleSelected == 'Parse':
-            self.DynamicFieldLabel1.SetLabel('Output field 1')
-            self.DynamicFieldLabel2.SetLabel('Output field 2')
-            # Remove fields that are not to be shown
-            self.RemoveDynamicElements()
-            
-            #Add correct fields
-            self.OutputField1       = wx.ComboBox(self.EditRulePanel,value="Artist", choices=self.OutputFields)
-            self.sizer1.Add(self.OutputField1)
-            self.OutputField2       = wx.ComboBox(self.EditRulePanel,value="Artist", choices=self.OutputFields)
-            self.sizer2.Add(self.OutputField2)
-
-            if self.Settings[u'Type'] == 'Parse':
-                self.OutputField1.SetStringSelection(self.Settings[u'Field3'])
-                self.OutputField2.SetStringSelection(self.Settings[u'Field4'])
-                self.TokenField.SetValue(self.Settings[u'Field2'])
-            else: 
-                self.OutputField1.SetStringSelection("Artist")
-                self.OutputField2.SetStringSelection("Title")
-                self.TokenField.SetValue("-")
-            # Show Fields
-            self.TokenLabel.Show() 
-            self.TokenField.Show() 
-
-        ##############################################      
-        if RuleSelected == 'Cortina':
-            self.DynamicFieldLabel1.SetLabel('is / is not')
-            self.DynamicFieldLabel2.SetLabel('Value(s)')
-            
-            # Remove fields that are not to be shown
-            self.RemoveDynamicElements()
-            
-            self.TokenLabel.Hide() 
-            self.TokenField.Hide() 
-            
-            #Add correct fields
-            self.IsIsNot    = wx.ComboBox(self.EditRulePanel,value="is", choices=["is", "is not"])
-            self.sizer1.Add(self.IsIsNot)
-            self.OutputField3 = wx.TextCtrl(self.EditRulePanel, value="", size=(165,-1))
-            self.sizer2.Add(self.OutputField3)
-            
-            if self.Settings[u'Type'] == 'Cortina':
-                self.IsIsNot.SetStringSelection(self.Settings[u'Field2'])
-                self.OutputField3.SetValue(self.Settings[u'Field3'])
-            else:
-                self.IsIsNot.SetStringSelection("is")
-                self.OutputField3.SetValue("")
-
-        self.EditRulePanel.Layout()
-
-    def RemoveDynamicElements(self):
-        try:
-            self.sizer1.Remove(self.OutputField1)
-            self.OutputField1.Hide()
-        except: pass
-        try:
-            self.sizer2.Remove(self.OutputField2)   
-            self.OutputField2.Hide()
-        except: pass
-        try:
-            self.sizer1.Remove(self.IsIsNot)
-            self.IsIsNot.Hide()
-        except: 
-            pass
-        try:
-            self.sizer2.Remove(self.OutputField3)   
-            self.OutputField3.Hide()
-        except: 
-            pass
-            
-            
-    def OnSaveRuleItem(self, event):
-        RuleOrderBox = int(self.RuleOrder.GetValue())-1
-        RuleSelected = self.RuleSelectDropdown.GetValue()
-        
-        # Build NewRule
-        NewRule = {}
-        NewRule[u'Type']        = RuleSelected
-        NewRule[u'Field1']      = self.InputID3Field.GetValue()
-        if self.ActivateRule.GetValue():
-            NewRule[u'Active']      = "yes"
-        else:
-            NewRule[u'Active']      = "no"
-            
-        if RuleSelected == 'Set':
-            NewRule[u'Field2']      = self.OutputField1.GetValue()
-        if RuleSelected == 'Parse':
-            NewRule[u'Field2']      = self.TokenField.GetValue()
-            NewRule[u'Field3']      = self.OutputField1.GetValue()
-            NewRule[u'Field4']      = self.OutputField2.GetValue()
-        if RuleSelected == 'Cortina':
-            NewRule[u'Field2']      = self.IsIsNot.GetValue()
-            NewRule[u'Field3']      = self.OutputField3.GetValue()
-        
-        # Decide where NewRule goes into the vector self.Settings
-        if self.mode == "Add rule":
-            if RuleOrderBox < self.RowSelected:
-                beamSettings._rules.insert(RuleOrderBox, NewRule) #Insert in at position
-            else:
-                beamSettings._rules.append(NewRule) # Append in the end
-        else: #Edit rule
-            if RuleOrderBox == self.RowSelected:
-                beamSettings._rules[RuleOrderBox] = NewRule # Overwrite
-            else:
-                beamSettings._rules.pop(self.RowSelected) #Move up and down in list
-                beamSettings._rules.insert(RuleOrderBox, NewRule)
-        self.parent.BuildRuleList()
-        self.Destroy()
-
-    def OnCancelRuleItem(self, event):
-        self.Destroy()
---
+++ trunk/bin/dialogs/editlayoutdialog.py
@@ -0,0 +1,117 @@
+import wx, wx.html
+import os
+
+from bin.beamsettings import *
+
+#
+# LAYOUT WINDOW-CLASS
+#
+#
+class EditLayoutDialog(wx.Dialog):
+    def __init__(self, parent, RowSelected, mode):
+        self.EditLayoutDialog   = wx.Dialog.__init__(self, parent, title=mode, size=(570,210))
+        self.EditLayoutPanel    = wx.Panel(self)
+        self.parent             = parent
+        self.RowSelected        = RowSelected
+
+        self.ButtonSaveLayout   = wx.Button(self.EditLayoutPanel, label="Save")
+        self.ButtonCancelLayout     = wx.Button(self.EditLayoutPanel, label="Cancel")
+        self.ButtonSaveLayout.Bind(wx.EVT_BUTTON, self.OnSaveLayoutItem)
+        self.ButtonCancelLayout.Bind(wx.EVT_BUTTON, self.OnCancelLayoutItem)
+
+        Fonts   = ["Decorative","Default","Modern","Roman","Script","Swiss","Teletype"]
+        Weights     = ["Bold","Light","Normal"]
+        Styles  = ["Italic","Normal","Slant"]
+        
+        # Check if it is a new line
+        if self.RowSelected<len(beamSettings._myDisplaySettings):
+            # Get the properties of the selected item
+            self.Settings   = beamSettings._myDisplaySettings[self.RowSelected]
+        else:
+            # Create a new default setting
+            self.Settings   = ({"Field": "Artist[1]", "Font": "Default","Style": "Normal", "Weight": "Bold", "Size": 20, "FontColor": "(255,255,255,255)", "HideControl": "NextTanda[1]", "Position": [50,50], "Center": "yes"})
+
+        
+        # Define fields
+        self.LabelText          = wx.TextCtrl(self.EditLayoutPanel, size=(150,-1), value=self.Settings[u'Field'])
+        self.FontDropdown       = wx.ComboBox(self.EditLayoutPanel,value=self.Settings[u'Font'], choices=Fonts)
+        self.StyleDropdown      = wx.ComboBox(self.EditLayoutPanel,value=self.Settings[u'Style'], choices=Styles)
+        self.WeightDropdown     = wx.ComboBox(self.EditLayoutPanel,value=self.Settings[u'Weight'], choices=Weights)
+        self.SizeText           = wx.TextCtrl(self.EditLayoutPanel, value=str(self.Settings[u'Size']))
+        self.ColorField         = wx.ColourPickerCtrl(self.EditLayoutPanel)
+        self.HideText           = wx.TextCtrl(self.EditLayoutPanel, value=self.Settings[u'HideControl'])
+        self.VerticalPos        = wx.TextCtrl(self.EditLayoutPanel, value=str(self.Settings[u'Position'][0]))
+        self.HorizontalPos      = wx.TextCtrl(self.EditLayoutPanel, value=str(self.Settings[u'Position'][1]))
+        #Checkbox
+        self.CenterCheck        = wx.CheckBox(self.EditLayoutPanel, label="Center")
+        self.CenterCheck.Bind(wx.EVT_CHECKBOX, self.DisableHorizontalBox)
+        if self.Settings[u'Center']=="yes": 
+            self.CenterCheck.SetValue(True)
+            self.HorizontalPos.Enable(not self.CenterCheck.GetValue())
+        # Set color
+        self.ColorField.SetColour(eval(self.Settings[u'FontColor']))
+        
+        # Information area
+        InfoGrid    =   wx.FlexGridSizer(4, 5, 5, 5)
+        InfoGrid.AddMany ( [(wx.StaticText(self.EditLayoutPanel, label="Label"), 0, wx.EXPAND),
+                        (wx.StaticText(self.EditLayoutPanel, label="Font"), 0, wx.EXPAND),
+                        (wx.StaticText(self.EditLayoutPanel, label="Style"), 0, wx.EXPAND),
+                        (wx.StaticText(self.EditLayoutPanel, label="Weight"), 0, wx.EXPAND),
+                        (wx.StaticText(self.EditLayoutPanel, label="Size"), 0, wx.EXPAND),
+                        (self.LabelText, 0, wx.EXPAND),
+                        (self.FontDropdown, 0, wx.EXPAND),
+                        (self.StyleDropdown, 0, wx.EXPAND),
+                        (self.WeightDropdown, 0, wx.EXPAND),
+                        (self.SizeText, 0, wx.EXPAND),
+                        (wx.StaticText(self.EditLayoutPanel, label="Hide/Show"), 0, wx.EXPAND),
+                        (wx.StaticText(self.EditLayoutPanel, label="Color"), 0, wx.EXPAND),
+                        (wx.StaticText(self.EditLayoutPanel, label="Vertical"), 0, wx.EXPAND),
+                        (wx.StaticText(self.EditLayoutPanel, label="Horizontal"), 0, wx.EXPAND),
+                        (wx.StaticText(self.EditLayoutPanel, label=""), 0, wx.EXPAND),
+                        (self.HideText, 0, wx.EXPAND),
+                        (self.ColorField, 0, wx.EXPAND),
+                        (self.VerticalPos, 0, wx.EXPAND),
+                        (self.HorizontalPos, 0, wx.EXPAND),
+                        (self.CenterCheck, 0, wx.EXPAND)
+                        ])
+
+        self.vboxLayout = wx.BoxSizer(wx.VERTICAL)
+        self.hboxLayout = wx.BoxSizer(wx.HORIZONTAL)
+        
+        self.hboxLayout.Add((400, -1), 1, flag=wx.EXPAND | wx.ALIGN_RIGHT)
+        self.hboxLayout.Add(self.ButtonSaveLayout, flag=wx.LEFT | wx.BOTTOM | wx.TOP, border=10)
+        self.hboxLayout.Add(self.ButtonCancelLayout, flag=wx.LEFT | wx.BOTTOM | wx.TOP | wx.RIGHT, border=10)
+        
+        self.vboxLayout.Add(InfoGrid, flag=wx.LEFT | wx.BOTTOM | wx.TOP | wx.RIGHT, border=10)
+        self.vboxLayout.Add(self.hboxLayout)
+
+        self.EditLayoutPanel.SetSizer(self.vboxLayout)
+        
+    def DisableHorizontalBox(self, event):
+        self.HorizontalPos.Enable(not self.CenterCheck.GetValue())
+        
+    def OnSaveLayoutItem(self, event):
+        self.Settings[u'Field']         = self.LabelText.GetValue()
+        self.Settings[u'Font']      = self.FontDropdown.GetValue()  
+        self.Settings[u'Style']         = self.StyleDropdown.GetValue()
+        self.Settings[u'Weight']        = self.WeightDropdown.GetValue()
+        self.Settings[u'Size']      = int(self.SizeText.GetValue())
+        self.Settings[u'HideControl']   = self.HideText.GetValue()
+        self.Settings[u'FontColor']     = str(self.ColorField.GetColour())
+        self.Settings[u'Position']  = [int(self.VerticalPos.GetValue()), int(self.HorizontalPos.GetValue())]
+        if self.CenterCheck.GetValue():
+            self.Settings[u'Center']    = 'yes'
+        else:
+            self.Settings[u'Center']    = 'no' 
+        
+        # Save item into dictionary
+        if self.RowSelected+1 > len(beamSettings._myDisplaySettings):
+            beamSettings._myDisplaySettings.append(self.Settings)
+        else:
+            beamSettings._myDisplaySettings[self.RowSelected] = self.Settings
+        self.parent.BuildLayoutList()
+        self.Destroy()
+    
+    def OnCancelLayoutItem(self, event):
+        self.Destroy()
+
