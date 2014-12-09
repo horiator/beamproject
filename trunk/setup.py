a6530d6501672df615f2d549cb2cb995902ad395
mikael.holber@gmail.com <mikael.holber@gmail.com@b2126cb8-cc4b-07e0-b2cb-7117c13d5ace>
2014-12-09 08:16:31 +0000

    Updated setup.py with optimization

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,5 +1,27 @@
 from distutils.core import setup
 import py2exe
- 
-setup(windows=['Beam.py'], options = {'py2exe':{'bundle_files':1}},ipfile = None,)
+
+includes = []
+excludes = ['_gtkagg', '_tkagg', 'bsddb', 'curses', 'email', 'pywin.debugger',
+	'pywin.debugger.dbgcon', 'pywin.dialogs', 'tcl',
+	'Tkconstants', 'Tkinter']
+
+packages = []
+dll_excludes = ['libgdk-win32-2.0-0.dll', 'libgobject-2.0-0.dll', 'tcl84.dll',
+	'tk84.dll']
+	
+setup(windows=['Beam.py'], 
+	options = {"py2exe": {"compressed": 2, 
+	"optimize": 2,
+	"includes": includes,
+	"excludes": excludes,
+	"packages": packages,
+	"dll_excludes": dll_excludes,
+	"bundle_files": 3,
+	"dist_dir": "dist",
+	"xref": False,
+	"skip_archive": False,
+	"ascii": False,
+	"typelibs": [('{9E93C96F-CF0D-43F6-8BA8-B807A3370712}',0,1,13)]}},
+	)
 	
