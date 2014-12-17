d7099852a92e3585714f0b7313e7f2dae601b974
mikael.holber@gmail.com <mikael.holber@gmail.com@b2126cb8-cc4b-07e0-b2cb-7117c13d5ace>
2014-12-17 18:41:43 +0000

    Updated reading/writing of settings to home-dir - part 2

---
+++ trunk/bin/Modules/spotifyWindowsModule.py
@@ -0,0 +1,90 @@
+# -*- encoding: utf-8 -*-
+#    Copyright (C) 2014 Mikael Holber http://mywebsite.com
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#    or download it from http://www.gnu.org/licenses/gpl.txt
+#
+#    Usage as function:
+#       Artist, Album, Title, Genre, Comment, Composer, Year, playbackStatus = audaciousModule(MaxTandaLength)
+#
+#    Example:
+#       Artist, Album, Title, Genre, Comment, Composer, Year, playbackStatus = audaciousModule.run(4)
+#
+#
+#    Revision History:
+#
+#    18/10/2014 Version 1.0
+#    	- Initial release
+#
+
+
+try:
+	import win32gui
+except ImportError:
+	pass
+	
+def run(MaxTandaLength):
+
+
+	# Variable declaration
+	
+	Artist 		= []
+	Album 		= []
+	Title	 		= []
+	Genre	 	= []
+	Comment	= []
+	Composer	= []
+	Year			= []
+
+	# Check if Spotify is running and create a handle
+	try:
+		spotify = win32gui.FindWindow("SpotifyMainWindow", None)
+	except:
+		playbackStatus = 'Mediaplayer is not running'
+		return Artist, Album, Title, Genre, Comment, Composer, Year, playbackStatus
+	# Check if closed
+	if win32gui.GetWindowText(spotify) == "":
+		playbackStatus = 'Mediaplayer is not running'
+		return Artist, Album, Title, Genre, Comment, Composer, Year, playbackStatus
+		
+	# Read from Spotify
+	try:
+		trackinfo = win32gui.GetWindowText(spotify).split(" - ")
+		artist, title = trackinfo[1].split(" \x96 ")
+		
+		# Create empty previous
+		Artist.append("")
+		Album.append("")
+		Title.append("")
+		Genre.append("")
+		Comment.append("")
+		Composer.append("")
+		Year.append("")
+		
+		Artist.append(artist)
+		Title.append(title)
+		Album.append("")
+		Genre.append("")
+		Comment.append("")
+		Composer.append("")
+		Year.append("")
+		
+		playbackStatus = 'Playing'
+		return Artist, Album, Title, Genre, Comment, Composer, Year, playbackStatus
+	
+	except:
+		playbackStatus = 'Paused'
+		return Artist, Album, Title, Genre, Comment, Composer, Year, playbackStatus
+
