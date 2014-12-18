374527192f0207278efca02dc5e0cd57f72d931e
horia.uifaleanu@gmail.com <horia.uifaleanu@gmail.com@b2126cb8-cc4b-07e0-b2cb-7117c13d5ace>
2014-12-18 09:28:26 +0000

    formatting

--- trunk/bin/Modules/itunesWindowsModule.py
+++ trunk/bin/Modules/itunesWindowsModule.py
@@ -1,5 +1,6 @@
-# -*- encoding: utf-8 -*-
-#    Copyright (C) 2014 Mikael Holber http://mywebsite.com
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#    Copyright (C) 2014 Mikael Holber http://http://www.beam-project.com
 #
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -16,84 +17,79 @@
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #    or download it from http://www.gnu.org/licenses/gpl.txt
 #
-#    Usage as function:
-#       Artist, Album, Title, Genre, Comment, Composer, Year, playbackStatus = audaciousModule(MaxTandaLength)
-#
-#    Example:
-#       Artist, Album, Title, Genre, Comment, Composer, Year, playbackStatus = audaciousModule.run(4)
-#
 #
 #    Revision History:
 #
-#    18/10/2014 Version 1.0
-#    	- Initial release
+#    XX/XX/2014 Version 1.0
+#       - Initial release
 #
+# This Python file uses the following encoding: utf-8
 
 import subprocess
 try:
-	import win32com.client
+    import win32com.client
 except ImportError:
-	pass
-	
+    pass
+    
 def run(MaxTandaLength):
 
 
-	# Variable declaration
-	
-	Artist 		= []
-	Album 		= []
-	Title	 		= []
-	Genre	 	= []
-	Comment	= []
-	Composer	= []
-	Year			= []
+    # Variable declaration
+    
+    Artist      = []
+    Album       = []
+    Title           = []
+    Genre       = []
+    Comment = []
+    Composer    = []
+    Year            = []
 
-		
-	# Check if iTunes is running and create a communications object
-	if ApplicationRunning("iTunes.exe"):
-		try:
-			itunes = win32com.client.gencache.EnsureDispatch ("iTunes.Application")
-		except:
-			playbackStatus = 'Mediaplayer is not running'
-			return Artist, Album, Title, Genre, Comment, Composer, Year, playbackStatus
-	else:
-		playbackStatus = 'Mediaplayer is not running'
-		return Artist, Album, Title, Genre, Comment, Composer, Year, playbackStatus
-	
-	
-	if itunes.PlayerState == 1:
-	#	print "Lets get some info!"
-		playbackStatus = 'Playing'
+        
+    # Check if iTunes is running and create a communications object
+    if ApplicationRunning("iTunes.exe"):
+        try:
+            itunes = win32com.client.gencache.EnsureDispatch ("iTunes.Application")
+        except:
+            playbackStatus = 'Mediaplayer is not running'
+            return Artist, Album, Title, Genre, Comment, Composer, Year, playbackStatus
+    else:
+        playbackStatus = 'Mediaplayer is not running'
+        return Artist, Album, Title, Genre, Comment, Composer, Year, playbackStatus
+    
+    
+    if itunes.PlayerState == 1:
+    #   print "Lets get some info!"
+        playbackStatus = 'Playing'
 
-		#Declare our position
-		currentsong	= itunes.CurrentTrack.PlayOrderIndex
+        #Declare our position
+        currentsong = itunes.CurrentTrack.PlayOrderIndex
 
-		# Extract previous song
-		if currentsong == 1:
-			searchsong = currentsong # Start on the current song
-		else:
-			searchsong = currentsong-1 # Start on previous song
-		
-		while searchsong < currentsong+MaxTandaLength+2:
-			try:
-				Track = itunes.CurrentTrack.Playlist.Tracks.Item(searchsong)
-				Artist.append((Track.Artist).encode('cp1250'))
-				Album.append((Track.Album).encode('cp1250'))
-				Title.append((Track.Name).encode('cp1250'))
-				Genre.append((Track.Genre).encode('cp1250'))
-				Comment.append((Track.Comment).encode('cp1250'))
-				Composer.append((Track.Composer).encode('cp1250'))
-				Year.append(Track.Year)
-				
-			except:
-				break
-			searchsong = searchsong+1
+        # Extract previous song
+        if currentsong == 1:
+            searchsong = currentsong # Start on the current song
+        else:
+            searchsong = currentsong-1 # Start on previous song
+        
+        while searchsong < currentsong+MaxTandaLength+2:
+            try:
+                Track = itunes.CurrentTrack.Playlist.Tracks.Item(searchsong)
+                Artist.append((Track.Artist).encode('cp1250'))
+                Album.append((Track.Album).encode('cp1250'))
+                Title.append((Track.Name).encode('cp1250'))
+                Genre.append((Track.Genre).encode('cp1250'))
+                Comment.append((Track.Comment).encode('cp1250'))
+                Composer.append((Track.Composer).encode('cp1250'))
+                Year.append(Track.Year)
+                
+            except:
+                break
+            searchsong = searchsong+1
 
-		return Artist, Album, Title, Genre, Comment, Composer, Year, playbackStatus
-	
-	else:
-		playbackStatus = 'Stopped'
-		return Artist, Album, Title, Genre, Comment, Composer, Year, playbackStatus
+        return Artist, Album, Title, Genre, Comment, Composer, Year, playbackStatus
+    
+    else:
+        playbackStatus = 'Stopped'
+        return Artist, Album, Title, Genre, Comment, Composer, Year, playbackStatus
 
 
 def ApplicationRunning(AppName):
