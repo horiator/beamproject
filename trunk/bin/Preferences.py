2c66bfa2e42c33d0d00fa7ac8ef2bd03078289ce
mikael.holber@gmail.com <mikael.holber@gmail.com@b2126cb8-cc4b-07e0-b2cb-7117c13d5ace>
2014-12-21 14:42:21 +0000

    Changed text in preferences

--- trunk/bin/Preferences.py
+++ trunk/bin/Preferences.py
@@ -85,7 +85,7 @@ class Preferences(wx.Dialog):
         Background = wx.StaticText(panel, -1, "Background Image", (10,87))
         font = wx.Font(11, wx.DEFAULT, wx.NORMAL, wx.BOLD)
         Background.SetFont(font)
-        wx.StaticText(panel, -1, "Select default background image (1920x1080 recommended)", (20,110))
+        wx.StaticText(panel, -1, "Select background image (1920x1080 recommended)", (20,110))
         self.browse = wx.Button(panel, label="Browse", pos=(20,129))
         self.browse.Bind(wx.EVT_BUTTON, self.browseBackgroundImage)
 
