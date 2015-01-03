440a3329537f9435f93e1baa22fb4ee60a4a07f1
horia.uifaleanu@gmail.com <horia.uifaleanu@gmail.com@b2126cb8-cc4b-07e0-b2cb-7117c13d5ace>
2015-01-03 10:25:52 +0000

    applyRules - to be moved into SongObject (nothing tested yet)

--- trunk/bin/songclass.py
+++ trunk/bin/songclass.py
@@ -32,7 +32,7 @@ class SongObject(object):
 
     def __init__(self, p_artist=None, p_album=None, p_title=None, p_genre=None
                  p_comment=None, p_composer=None, p_year=None, p_singer=None
-                 p_albumArtist=None, p_isCortina = None):
+                 p_albumArtist=None, p_performer = None, p_isCortina = None):
         self._artist      = p_artist
         self._album       = p_album
         self._title       = p_title
@@ -42,6 +42,7 @@ class SongObject(object):
         self._year        = p_year
         self._singer      = p_singer
         self._albumArtist = p_albumArtist
+        self._performer   = p_performer
         self._isCortina   = p_isCortina
         
     def __eq__(self, other)
@@ -53,7 +54,8 @@ class SongObject(object):
                     self._comment = other._comment &&
                     self._composer == other._composer &&
                     self._year == other._year &&
-                    self._albumArtist == other._albumArtist)
+                    self._albumArtist == other._albumArtist &&
+                    self._performer == other._performer)
         else:
             return false
             
@@ -67,8 +69,58 @@ class SongObject(object):
                     self._comment != other._comment ||
                     self._composer != other._composer ||
                     self._year != other._year ||
-                    self._albumArtist != other._albumArtist)
+                    self._albumArtist != other._albumArtist ||
+                    self._performer != other._performer)
         else:
-            return false    
+            return false 
+              
+    def applyRules(self, rulesArray)
+        for i in range(0, len(rulesArray)):
+            currentRule = rulesArray[i]
+            try:
+                if currentRule[u'Type'] == 'Parse' and currentRule[u'Active'] == 'yes':
+                    # Find Rule[u'Field2'] in Rule[u'Field1'],
+                    # split Rule[u'Field1'] and save into Rule[u'Field3 and 4]
+                    if str(Rule[u'Field2'].replace("%"," self.")) in str(eval(Rule[u'Field1'].replace("%"," self."))):
+                        splitStrings = eval(str(Rule[u'Field1']).replace("%"," self."))[j].split(str(Rule[u'Field2']))
+                        [eval(Rule[u'Field3'].replace("%"," self.")), eval(Rule[u'Field4'].replace("%"," self."))] = [splitStrings[0], splitStrings[1]]
+
+                if Rule[u'Type'] == 'Cortina' and Rule[u'Active'] == 'yes':
+                    # Rule[u'Field2'] == is: IsCortina[j] shall be 1 if Rule[u'Field1'] is Rule[u'Field3']
+                    if Rule[u'Field2'] == 'is':
+                        if eval(str(Rule[u'Field1']).replace("%"," self.")) in str(Rule[u'Field3']):
+                            self.IsCortina[j] = 1
+                    # Rule[u'Field2'] == is not: IsCortina[j] shall be 1 if Rule[u'Field1'] not in Rule[u'Field3']
+                    if Rule[u'Field2'] == 'is not':
+                        if eval(str(Rule[u'Field1']).replace("%"," self.")) not in str(Rule[u'Field3']):
+                            self.IsCortina[j] = 1
+
+                if Rule[u'Type'] == 'Copy' and Rule[u'Active'] == 'yes':
+                    # Rule[u'Field1'] shall be Rule[u'Field2']
+                    # Example:
+                    # Singer(j) = Comment(j)
+                    eval(str(Rule[u'Field2']).replace("%"," self.")) = eval(str(Rule[u'Field1']).replace("%"," self."))
+
+                if Rule[u'Type'] == 'Mood' and Rule[u'Active'] == 'yes':
+                    # Only apply Mood for current song (j==1)
+                    if Rule[u'Field2'] == 'is':
+                        if eval(str(Rule[u'Field1']).replace("%"," self.")) in str(Rule[u'Field3']) and str(Rule[u'PlayState']) in self.PlaybackStatus and j == 1:
+                            self.CurrentMood = Rule[u'Name']
+                            self.DisplaySettings = Rule[u'Display']
+                            self.BackgroundImage = Rule[u'Background']
+                    if Rule[u'Field2'] == 'is not':
+                        if eval(str(Rule[u'Field1']).replace("%"," self.")) not in str(Rule[u'Field3']) and str(Rule[u'PlayState']) in self.PlaybackStatus and j == 1:
+                            self.CurrentMood = Rule[u'Name']
+                            self.DisplaySettings = Rule[u'Display']
+                            self.BackgroundImage = Rule[u'Background']                              
+                    # Only if playback is stopped and we have a mood for this
+                    if self.PlaybackStatus == "Stopped":
+                        if eval(str(Rule[u'Field1']).replace("%"," self.")) in str(Rule[u'Field2']) and str(Rule[u'PlayState']) in self.PlaybackStatus:
+                            self.CurrentMood = Rule[u'Name']
+                            self.DisplaySettings = Rule[u'Display']
+                            self.BackgroundImage = Rule[u'Background']
+            except:
+                print "Error at Rule:", i,".Type:", Rule[u'Type'], ". First Field", Rule[u'Field1']
+                break
     
     
