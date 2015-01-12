fe1bf027c6166ab98b40200abbe204c4a97aefca
horia.uifaleanu@gmail.com <horia.uifaleanu@gmail.com@b2126cb8-cc4b-07e0-b2cb-7117c13d5ace>
2015-01-12 21:51:42 +0000

    Spotify-Unix module working using the new songclass

--- bin/Modules/spotifyLinuxModule.py
+++ bin/Modules/spotifyLinuxModule.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python
-# -*- coding: <<encoding>> -*-
-#    Copyright (C) 2014 Mikael Holber http://mywebsite.com
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#    Copyright (C) 2014 Mikael Holber http://http://www.beam-project.com
 #
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -17,18 +17,17 @@
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #    or download it from http://www.gnu.org/licenses/gpl.txt
 #
-#    Usage as function:
-#       Artist, Album, Title, Genre, Comment, Composer, Year, playbackStatus = audaciousModule(MaxTandaLength)
-#
-#    Example:
-#       Artist, Album, Title, Genre, Comment, Composer, Year, playbackStatus = audaciousModule.run(4)
-#
 #
 #    Revision History:
 #
-#    18/10/2014 Version 1.0
-#    	- Initial release
+#    XX/XX/2014 Version 1.0
+#       - Initial release
 #
+# This Python file uses the following encoding: utf-8
+
+
+from bin.songclass import SongObject
+
 import imp
 try:
     imp.find_module('dbus') #doesn't exist in Windows
@@ -36,52 +35,81 @@ try:
 except ImportError:
     found = False
     
+from ID3 import *
+
 def run(MaxTandaLength):
 
-	Artist 		= []
-	Album 		= []
-	Title	 		= []
-	Genre	 	= []
-	Comment		= []
-	Composer		= []
-	Year			= []
-	playbackStatus  = ''
+    playlist = []
+    playbackStatus  = ''
 
-	try:
-		bus = dbus.SessionBus()
-		spotify_bus = bus.get_object("org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2")
-		spotify = dbus.Interface(spotify_bus,"org.mpris.MediaPlayer2.Player")
-		properties_manager = dbus.Interface(spotify_bus, 'org.freedesktop.DBus.Properties')
-		metadata = properties_manager.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
-		playbackStatus = properties_manager.Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus')
-	except:
-		playbackStatus  = 'Media player not running'
-		return Artist, Album, Title, Genre, Comment, Composer, Year, playbackStatus
-		
-	if playbackStatus == 'Paused':
-		return Artist, Album, Title, Genre, Comment, Composer, Year, playbackStatus
+    try:
+        bus = dbus.SessionBus()
+        spotify_bus = bus.get_object("org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2")
+        spotify = dbus.Interface(spotify_bus,"org.mpris.MediaPlayer2.Player")
+        properties_manager = dbus.Interface(spotify_bus, 'org.freedesktop.DBus.Properties')
+        metadata = properties_manager.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
+        playbackStatus = properties_manager.Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus')
+    except:
+        playbackStatus  = 'PlayerNotRunning'
+        return playlist, playbackStatus
 
-	# Retrieve current song
-	if playbackStatus == 'Playing':
+    # Retrieve current song
+    if playbackStatus == 'Playing':
+        playlist.append( getSongObjectFromTrack(metadata) )
+        
+    return playlist, playbackStatus
 
-		try:
-			Artist.append(metadata['xesam:artist'][0].encode('utf-8'))
-		except:
-			Artist.append('')
-		try:
-			Title.append(metadata['xesam:title'].encode('utf-8'))
-		except:
-			Title.append('')
-		try:
-			Album.append(metadata['xesam:album'].encode('utf-8'))
-		except:
-			Album.append('')
-		Genre.append('')
-		Comment.append('')
-		Composer.append('')
-		try:
-			Year.append(metadata['xesam:contentCreated'][:4])
-		except:
-			Year.append('')
 
-	return Artist, Album, Title, Genre, Comment, Composer, Year, playbackStatus
+def getSongObjectFromTrack(metadata):
+    retSong = SongObject()
+
+    try:
+        retSong.Artist      = (metadata[u'xesam:artist'])[0].encode('utf-8')
+    except:
+        pass
+        
+    try:
+        retSong.Album       = metadata['xesam:album'].encode('utf-8')
+    except:
+        pass
+    
+    try:
+        retSong.Title       = metadata['xesam:title'].encode('utf-8')
+    except:
+        pass
+        
+    try:
+        retSong.Genre       = (metadata['xesam:genre'])[0].encode('utf-8')
+    except:
+        pass
+        
+    try:
+        retSong.Comment     = (metadata['xesam:comment'])[0].encode('utf-8')
+    except:
+        pass
+        
+    try:
+        retSong.Composer    = (Track[u'composer']).encode('utf-8')
+    except:
+        pass
+        
+    try:
+        retSong.Year        = (metadata['xesam:contentCreated'])[:4].encode('utf-8')
+    except:
+        pass
+        
+    #retSong.Singer
+    
+    try:
+        retSong.AlbumArtist = ""
+    except:
+        pass
+        
+    try:
+        retSong.Performer   = ""
+    except:
+         pass
+     #retSong.IsCortina
+    
+    return retSong
+
