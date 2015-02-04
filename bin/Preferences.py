327d1c7989f0398ebbbd0c63c15fea095780c56b
mikael.holber@gmail.com <mikael.holber@gmail.com@b2126cb8-cc4b-07e0-b2cb-7117c13d5ace>
2015-02-04 16:04:20 +0000

    Fixed issue 45: Same update as for trunk.

--- bin/Preferences.py
+++ bin/Preferences.py
@@ -200,11 +200,11 @@ class Preferences(wx.Dialog):
                 self.RuleRows.append(str('Copy '+rule[u'Field1']+' to '+rule[u'Field2']))
             if rule[u'Type'] == "Cortina":
                 if rule[u'Field2'] =="is":
-                    self.RuleRows.append(str('Its a Cortina when: '+rule[u'Field1']+' is '+rule[u'Field3']))
+                    self.RuleRows.append(str("It's a Cortina when: "+rule[u'Field1']+' is '+rule[u'Field3']))
                 if rule[u'Field2'] =="is not":
-                    self.RuleRows.append(str('Its a Cortina when: '+rule[u'Field1']+' is not '+rule[u'Field3']))
+                    self.RuleRows.append(str("It's a Cortina when: "+rule[u'Field1']+' is not '+rule[u'Field3']))
                 if rule[u'Field2'] =="contains":
-                    self.RuleRows.append(str('Its a Cortina when: '+rule[u'Field1']+' contains '+rule[u'Field3']))
+                    self.RuleRows.append(str("It's a Cortina when: "+rule[u'Field1']+' contains '+rule[u'Field3']))
             if rule[u'Type'] == "Parse":
                 self.RuleRows.append(str('Parse/split '+rule[u'Field1']+' containing '+rule[u'Field2']+' into '+rule[u'Field3']+' and '+rule[u'Field4']))
         self.RuleList.Set(self.RuleRows)
